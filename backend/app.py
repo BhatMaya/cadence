ba5d09d0ad58becc3b8c7846d810e7a332946dc2
Maya <mayabhat@cs.washington.edu>
2026-06-07 15:33:43 -0700

    added patch allowance for threshold

--- backend/app.py
+++ backend/app.py
@@ -82,7 +82,7 @@ def add_cors_headers(response):
         response.headers["Access-Control-Allow-Origin"] = origin
         response.headers["Vary"] = "Origin"
         response.headers["Access-Control-Allow-Credentials"] = "true"
-        response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
+        response.headers["Access-Control-Allow-Methods"] = "GET,POST,PATCH,OPTIONS"
         response.headers["Access-Control-Allow-Headers"] = (
             "Content-Type, Authorization, X-Cadence-Admin-Token"
         )
